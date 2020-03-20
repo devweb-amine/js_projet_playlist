b003be540ca5f013a5c50e5750d3d764f06f2808
devweb-amine <58747816+devweb-amine@users.noreply.github.com>
2020-03-20 11:33:19 +0100

    exo1

--- 1-decouvrir-DOM-jquery/niveau1/script.js
+++ 1-decouvrir-DOM-jquery/niveau1/script.js
@@ -1,1 +1,21 @@
+$(document).ready(function()
+{  
+   $('button').click(function()
+   {   
+      film = $.get('./playlist.txt', function(data)
+       {  
+         var datasplited = (data.split('\n')) ;
 
+         $.each(datasplited , function(index, value)
+
+
+           {  
+             $('#list').append("<li>"+value+"</li>");
+
+
+
+           });
+
+        });
+   });
+});
